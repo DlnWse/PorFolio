765feae6f96c53cc298213bd49d6fead191b7154
DlnWse <dylan.weisse@hotmail.com>
2020-09-29 09:34:23 +0200

    suite

--- PHP/index.php
+++ PHP/index.php
@@ -35,19 +35,40 @@
 
 
     </div>
-    <div id="about "></div>
-    <div  class="apropos">
-        <div class="boxL tGray">
+    <div id="about"></div>
+
+    <div class="apropos">
+        <div class="boxL">
             <h1 class="titlepart">A Propos</h1>
 
         </div>
-<div class="boxR tWhite">
+        <div class="boxR">
 
-</div>
+            <h1 class="titlepart">Qui suis-je ?</h1>
+
+           <p class="center"> Hey ! Moi c'est Dylan, et je suis Developpeur WEB Junior. <br><br>
+
+                Si vous êtes ici, ce n'est pas pour rien ! Alors laissez moi vous raconter mon parcours. <br><br></p>
+
+               <p class="justify"> Avant d'en arriver là, le métier qui m'était destiné n'étais pas le WEB, mais plutôt les métiers du batiment. Mais le soucis était que l'épanouissement au travail n'était pas là, et c'est important !
+
+                Je me suis donc remis en question et cherché quelque-chose qui me plaisait: L'informatique ! En général pour commencer, puis en me spécialisant. <br><br>
+
+                J'ai été formé dans la bureautique, pour obtenir mon PCIE, et par la suite former des personnes comme moi, c'était très bien mais il manquait encore un petit quelque chose.
+
+                Alors j'ai essayé le développement, et là j'ai pu m'y retrouver, et trouver par la même occasion ce qui me passionnais:
+                "Le WEB"<br><br>
+
+                Vous voulez en voir plus ?</p>
+
+
+
+
+        </div>
 
     </div>
 
-    <!-- <div class="comp">
+    <div class="comp">
 
     </div>
 
@@ -57,7 +78,7 @@
 
     <div class="contact">
 
-    </div> -->
+    </div>
 
 
 
